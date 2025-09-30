fea9989a55181e369c9d0f567d36942ccb893c22
GreatEmon <contact.emonpro@gmail.com>
2025-10-01 02:58:52 +0600

    Edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     });
   }
 
-  // if(loading) return <Loading />
+  if(loading) return <Loading />
   return (
     <nav className="bg-base-100 shadow-md">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
